d7479f53b48fd1b2aca606a9d5d3338c9d40a019
Prisha <prishabhasin14@gmail.com>
2025-02-25 15:27:43 +0530

    added options as constant

---
+++ src/constants/options.jsx
@@ -0,0 +1,53 @@
+export const SelectTravelesList=[
+    {
+        id:1,
+        title:'Just Me',
+        desc:'A sole traveles in exploration',
+        icon:'✈️',
+        people:'1'
+    },
+    {
+        id:2,
+        title:'A couple',
+        desc:'Two traveles in tandem',
+        icon:'🥂',
+        people:'2 People'
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:'A group of fun loving adv',
+        icon:'🏠',
+        people:'3 to 5 people'
+    },
+    {
+        id:4,
+        title:'Friends',
+        desc:'Abunch of thrill-seeks',
+        icon:'🏄‍♂️',
+        people:'5 to 10 people'
+    }
+]
+
+export const SelectBudgetOptions=[
+    {
+        id:1,
+        title:'Cheap',
+        desc:'A group of fun loving adv',
+        icon:'🏠',
+    },
+    {
+        id:2,
+        title:'Moderate',
+        desc:'Keep cost on the average side',
+        icon:'💵',
+    },
+    {
+        id:3,
+        title:'Luxury',
+        desc:'Dont worry about cost',
+        icon:'💰',
+    }
+]
+
+export const AI_PROMPT='Generate Travel Plan for Location:{location}, for {totalDays} Days for {traveler} with a {budget} budget, give me Hotels options list with HotelName, Hotel address, Prie, hotel image url, geo coordinates, rating, descriptions and suggest itinerary with plaeName, Place Details, Place Image Url, Geo Coordinatwa, ticket Pricing, Time Travel each ofthe location for {totalDays} days with each day plan with best time to visit in JSON format.'
